f1d25353f6153782814a5dcc1d292304ce226f34
doyodoyo <doyo@kanagawa-u.ac.jp>
2026-02-01 23:32:20 +0900

    フォーマットrev1

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template, request, session, redirect,flash, url_for
+
+app = Flask(__name__)
+
+
+# 編集ゾーンーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+##ここでルートのURLが指定された時に実行される
+@app.route('/')
+def index():
+    ##index.htmlがレンダリングされる
+    return render_template("index.html")
+
+# ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
+
